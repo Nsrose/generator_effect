8b4d2e22cc0da9ad79a8d6116f1c6169e051fd61
nsrose <nsrose@berkeley.edu>
2014-03-01 15:10:13 -0800

    modified templates

--- generator_site/app/views.py
+++ generator_site/app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template 
 from app import app
+from logic import * 
 
 @app.route('/')
 @app.route('/index')
@@ -9,3 +10,15 @@ def index():
 @app.route('/result')
 def result():
 	return render_template("result.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/creators')
+def creators():
+	return render_template("creators.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
