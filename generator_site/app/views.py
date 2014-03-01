2a0d998061b1e53bd4667b2fc3af3772f1605941
nsrose <nsrose@berkeley.edu>
2014-03-01 14:53:15 -0800

    modified templates

--- generator_site/app/views.py
+++ generator_site/app/views.py
@@ -1,6 +1,11 @@
+from flask import render_template 
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("home.html")
+
+@app.route('/result')
+def result():
+	return render_template("result.html")
